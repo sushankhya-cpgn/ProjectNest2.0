ef3bd04c8642b75c35af67e801d0ff2fb53a3508
sushankhya-cpgn <chapagain.sushankhya@gmail.com>
2024-05-05 16:59:43 +0545

    Routes added for login and app

--- Client_ProjectNest/src/App.jsx
+++ Client_ProjectNest/src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 function App() {
   return (
-    <div className="text-xl font-bold underline text-green-500 ">
-      {" "}
-      ProjectNest
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Navigate to="/login" replace />} />
+          <Route path="login" element={<p>Login</p>} />
+        </Route>
+        <Route path="app" element={<p>App is great</p>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
