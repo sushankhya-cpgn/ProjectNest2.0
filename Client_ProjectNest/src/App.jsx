a007f0ba5a2a2d962d5845e26d37a55a99642980
Mohit Shahi <moheetshahi92@gmail.com>
2024-06-03 18:55:12 +0545

    app route

--- Client_ProjectNest/src/App.jsx
+++ Client_ProjectNest/src/App.jsx
@@ -19,7 +19,6 @@ import FindPorjectProjectDetail from "./components/student/FindPorjectProjectDet
 import ProjectPage from "./pages/ProjectPage";
 import Profile from "./components/Profile";
 import ProjectsPage from "./pages/ProjectsPage";
-
 import Task from "./components/supervisor/Task";
 import Logsheet from "./components/supervisor/Logsheet";
 import GroupChat from "./components/supervisor/GroupChat";
@@ -54,30 +53,11 @@ function App() {
                   <Route path="findprojects" element={<FindProjects />} />
                   <Route path="settings" element={<Settings />} />
                 </Route>
-                <Route path="settings" element={<Setting />} />
-              </Route>
-              <Route path="supervisor" element={<SupervisorDashboard />}>
-                <Route index element={<Navigate to="homesuper" replace />} />
-                <Route path="homesuper" element={<SuperFeed />} />
-                <Route path="findprojects" element={<FindProjects />} />
-                <Route path="settings" element={<Settings />} />
-              </Route>
-              <Route
-                path="supervisor/projects/:projectId"
-                element={<ProjectsPage />}
-              >
-                <Route index element={<Navigate to="tasks" replace />} />
-                <Route path="tasks" element={<Task />} />
-                <Route path="member" element={<Members />} />
-                <Route path="logsheets" element={<Logsheet />} />
-                <Route path="chats" element={<GroupChat />} />
-                <Route path="documents" element={<Documents />} />
-              </Route>
-              <Route path="admin" element={<AdminPage />}>
                 <Route
                   path="supervisor/projects/:projectId"
                   element={<ProjectsPage />}
                 >
+                  <Route index element={<Navigate to="tasks" replace />} />
                   <Route path="tasks" element={<Task />} />
                   <Route path="member" element={<Members />} />
                   <Route path="logsheets" element={<Logsheet />} />
@@ -89,7 +69,6 @@ function App() {
                     index
                     element={<Navigate to="projectrequests" replace />}
                   />
-
                   <Route path="projectrequests" element={<ProjectRequests />} />
                   <Route path="project">
                     <Route index element={<Navigate to="projectdetails" />} />
@@ -97,13 +76,11 @@ function App() {
                       path="projectdetails"
                       element={<AdminProjectDetails />}
                     />
-
                     <Route
                       path="projectdetails/projectdetail/:projectID"
                       element={<ProjectPage />}
                     />
                   </Route>
-
                   <Route path="deletedprojects" element={<Deletedprojects />} />
                   <Route path="archives" element={<Archieves />} />
                 </Route>
