f38dd263eb4a09eb02dc540e02c6057acb1ad7af
Ravi Pajiyar <pajiyargravi20011@gmail.com>
2024-06-03 17:48:38 +0545

    document form and file ui completed

---
+++ Client_ProjectNest/src/components/supervisor/DocForm.jsx
@@ -0,0 +1,185 @@
+import Datepicker from "tailwind-datepicker-react";
+import { useState } from "react";
+
+const options = {
+  title: "Pick Date",
+  autoHide: true,
+  todayBtn: false,
+  clearBtn: true,
+  clearBtnText: "Clear",
+  maxDate: new Date("2030-01-01"),
+  minDate: new Date("1950-01-01"),
+  theme: {
+    background: "bg-gray-700 dark:bg-gray-800",
+    todayBtn: "text-text ",
+    clearBtn:
+      "bg-primary text-text border-none hover:bg-slate-600 transition-all duration-200",
+    icons:
+      "bg-primary text-text hover:bg-slate-600 transition-all duration-200",
+    text: "text-text font-normal ",
+    disabledText: "bg-text-gray-600",
+    input: "",
+    inputIcon: "",
+    selected: "bg-red-500 text-text",
+  },
+  icons: {
+    prev: () => <span className="text-text font-bold">&larr;</span>,
+    next: () => <span className="text-text font-bold">&rarr;</span>,
+  },
+  datepickerClassNames: "top-12",
+  defaultDate: new Date("2024-05-23"),
+  language: "en",
+  disabledDates: [],
+  weekDays: ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+  inputNameProp: "date",
+  inputIdProp: "date",
+  inputPlaceholderProp: "Select Date",
+  inputDateFormatProp: {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  },
+};
+
+export default function DocForm() {
+  const initialData = [
+    {
+      id: 1,
+      name: "",
+      type: "",
+      members: "",
+    },
+  ];
+  const projectOptions = ["rentNread"];
+  const memberOptions = [
+    "Ravi Pajiyar",
+    "Mohit Shahi",
+    "Arun Bhandari",
+    "Sushankhya Chapa",
+  ];
+  const resultOptions = ["Weekly", "Final"];
+  const [show, setShow] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [data, setData] = useState(initialData);
+  const [inputs, setInputs] = useState([""]);
+
+  const handleSelectChange = (id, field, value) => {
+    setData((prevData) =>
+      prevData.map((row) => (row.id === id ? { ...row, [field]: value } : row))
+    );
+  };
+
+  const handleChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const handleClose = (state) => {
+    setShow(state);
+  };
+
+  return (
+    <div className="taskform flex flex-col text-base gap-4 mt-4 px-8">
+      <div className="section1 flex justify-between">
+        <span className="text-text font-medium">Projects</span>
+        {data.map((row) => (
+          <span className="text-text" key={row.id}>
+            <select
+              value={row.name}
+              onChange={(e) =>
+                handleSelectChange(row.id, "name", e.target.value)
+              }
+              className="bg-primary border-none rounded-md p-1.5 focus:outline-none focus:ring-0"
+            >
+              <option value="">project</option>
+              {projectOptions.map((name) => (
+                <option key={name} value={name}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </span>
+        ))}
+      </div>
+      <div className="section2 flex justify-between">
+        <span className="text-text font-medium">Result Type</span>
+        {data.map((row) => (
+          <span className="text-text" key={row.id}>
+            <select
+              value={row.type}
+              onChange={(e) =>
+                handleSelectChange(row.id, "type", e.target.value)
+              }
+              className="bg-primary border-none rounded-md p-1.5 focus:outline-none focus:ring-0"
+            >
+              <option value="">result</option>
+              {resultOptions.map((result) => (
+                <option key={result} value={result}>
+                  {result}
+                </option>
+              ))}
+            </select>
+          </span>
+        ))}
+      </div>
+      <div className="section3 flex justify-between">
+        <span className="text-text font-medium">Members</span>
+        {data.map((row) => (
+          <span className="text-text" key={row.id}>
+            <select
+              value={row.members}
+              onChange={(e) =>
+                handleSelectChange(row.id, "members", e.target.value)
+              }
+              className="bg-primary border-none rounded-md p-1.5 focus:outline-none focus:ring-0"
+            >
+              <option value="">member</option>
+              {memberOptions.map((members) => (
+                <option key={members} value={members}>
+                  {members}
+                </option>
+              ))}
+            </select>
+          </span>
+        ))}
+      </div>
+      <div className="section4 flex justify-between">
+        <span className="text-text font-medium">Created on</span>
+        <span className="w-3/5">
+          <Datepicker
+            options={options}
+            onChange={handleChange}
+            show={show}
+            setShow={handleClose}
+          />
+        </span>
+      </div>
+      <div className="section5 flex flex-col p-2 bg-primary rounded-lg gap-2">
+        <span className="text-text font-medium">Result</span>
+        <div className="inputs flex flex-col">
+          <input
+            type="file"
+            className="rounded-md px-2 py-0.5 border-none focus:outline-none focus:ring-0"
+          />
+        </div>
+      </div>
+      <div className="section6 flex flex-col p-2 bg-primary rounded-lg gap-2">
+        <span className="text-text font-medium">Remarks</span>
+        <div className="inputs flex flex-col">
+          <input
+            type="text"
+            placeholder="remark"
+            className="rounded-md px-2 py-0.5 border-none focus:outline-none focus:ring-0"
+          />
+        </div>
+      </div>
+      <div className="buttons flex justify-between">
+        <button className="bg-accent px-5 py-3 rounded-lg flex items-center justify-center gap-2 h-9 text-text">
+          Edit
+        </button>
+        <button className="bg-accent px-5 py-3 rounded-lg flex items-center justify-center gap-2 h-9 text-text">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
--- Client_ProjectNest/src/components/supervisor/Documents.jsx
+++ Client_ProjectNest/src/components/supervisor/Documents.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaFilter } from "react-icons/fa6";
 import { FaSortAmountUp } from "react-icons/fa";
+import DocForm from "./DocForm";
+import File from "./File";
+import RecentFile from "./RecentFile";
 
 export default function Documents() {
+  const [openNew, setOpenNew] = useState(false);
+
+  const handleClick = () => {
+    setOpenNew(!openNew);
+  };
+
+  const handleClose = () => {
+    setOpenNew(false);
+  };
   return (
     <div className="w-full flex justify-center items-center p-2">
       <div className="w-full h-full rounded-lg p-6 relative overflow-scroll flex flex-col gap-3">
         <div className="heading text-text font-semibold text-xl">Documents</div>
         <div className="w-full border-[0.5px] border-gray-300 "></div>
-        <div className="buttons flex gap-6 mt-2">
-          <button className="bg-accent px-5 py-3 rounded-lg flex items-center justify-center gap-3 h-10 text-text">
+        <div className="buttons flex gap-6 mt-3">
+          <button
+            className="bg-accent px-5 py-3 rounded-lg flex items-center justify-center gap-3 h-10 text-text"
+            onClick={handleClick}
+          >
             <span className="text-xl">+</span>
             <span className="hidden sm:block sm:text-sm">New</span>
           </button>
@@ -27,25 +42,37 @@ export default function Documents() {
           </button>
         </div>
         <div className="folders flex flex-col gap-3 mt-5">
-          <span className="text-text font-semibold text-xl">Folders</span>
-          <div className="boxes flex gap-5">
-            <div className="w-40 h-40 rounded-md bg-secondary text-text"></div>
-            <div className="w-40 h-40 rounded-md bg-secondary text-text"></div>
-            <div className="w-40 h-40 rounded-md bg-secondary text-text"></div>
-            <div className="w-40 h-40 rounded-md bg-secondary text-text"></div>
-            <div className="w-40 h-40 rounded-md bg-secondary text-text"></div>
+          <span className="text-text font-semibold text-xl">Files</span>
+          <div className="boxes flex gap-5 overflow-y-scroll whitespace-nowrap">
+            <File />
+            <File />
+            <File />
           </div>
         </div>
         <div className="recent flex flex-col gap-3 mt-5">
           <span className="text-text font-semibold text-xl">Recent</span>
           <div className="boxes flex gap-5">
-            <div className="w-44 h-20 rounded-md bg-secondary text-text"></div>
-            <div className="w-44 h-20 rounded-md bg-secondary text-text"></div>
-            <div className="w-44 h-20 rounded-md bg-secondary text-text"></div>
-            <div className="w-44 h-20 rounded-md bg-secondary text-text"></div>
+            <RecentFile />
+            <RecentFile />
           </div>
         </div>
       </div>
+      {openNew && (
+        <>
+          <div className="fixed inset-0 backdrop-blur-sm"></div>
+          <div className="fixed inset-0 flex justify-center items-center">
+            <div className="bg-secondary w-1/3 h-5/6 rounded-lg p-6 relative">
+              <button
+                className="absolute top-2 right-3 text-text font-bold text-xl hover:text-gray-400"
+                onClick={handleClose}
+              >
+                X
+              </button>
+              <DocForm />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
